99ab876d41ef0a89ddd9fba5a24dd955b8e4ad15
Igor Ivanov <ivanov.i@gmail.com>
2021-01-10 11:36:44 +0700

    m1 fzf

--- fzf.zsh
+++ fzf.zsh
@@ -1,18 +1,16 @@
-if [[ "$OSTYPE" == "darwin"* ]]; then
-		# Mac OSX
-		# Setup fzf
-		# ---------
-		if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-				export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
-		fi
-
-		# Auto-completion
-		# ---------------
-		[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-		# Key bindings
-		# ------------
-		source "/usr/local/opt/fzf/shell/key-bindings.zsh"
-else
-		export FZF_BASE="$(dirname "$(which fzf)")"
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
+
+# export FZF_BASE="$(dirname "$(which fzf)")"
+export FZF_BASE="/opt/homebrew/opt/fzf"
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
