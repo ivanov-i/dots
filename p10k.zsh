7e0173cdafbe5b2a9c7ff16f2582a3b1df069917
Igor Ivanov <ivanov.i@gmail.com>
2022-06-22 07:43:33 +0700

    remove "took" word from the prompt

--- p10k.zsh
+++ p10k.zsh
@@ -545,7 +545,7 @@
   # Custom icon.
   # typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_VISUAL_IDENTIFIER_EXPANSION='⭐'
   # Custom prefix.
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PREFIX='%ftook '
+  # typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PREFIX='%ftook '
 
   #######################[ background_jobs: presence of background jobs ]#######################
   # Don't show the number of background jobs.
