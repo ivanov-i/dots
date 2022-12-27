03a1ac65e2f41d12f1f6c2b2843c56c3e77fb405
Igor Ivanov <ivanov.i@gmail.com>
2022-12-27 17:00:10 +0700

    compatibility with neovim 0.7

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -1,6 +1,6 @@
 function ColorMyPencils(color)
 	color = color or "distilled"
-	vim.cmd.colorscheme(color)
+	vim.cmd("colorscheme " .. color)
 
 	vim.api.nvim_set_hl(0, "Normal", {bg = "None"})
 	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "None"})
--- config/nvim/lua/everything/remap.lua
+++ config/nvim/lua/everything/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("i", "jj", "<escape>")
-vim.keymap.set("n", "<cr>", vim.cmd.nohl)
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<cr>", ":nohl<cr>")
+vim.keymap.set("n", "<leader>pv", ":Ex<cr>")
 vim.keymap.set("n", "<space>", "<nop>")
 
 -- move by screen lines when wrapping
@@ -11,6 +11,6 @@ vim.keymap.set("n", "j", "gj")
 vim.keymap.set("n", "k", "gk")
 
 -- buffers
-vim.keymap.set("n", "]b", vim.cmd.bnext)
-vim.keymap.set("n", "[b", vim.cmd.bprev)
+vim.keymap.set("n", "]b", ":bnext<cr>")
+vim.keymap.set("n", "[b", ":bprev<cr>")
 
