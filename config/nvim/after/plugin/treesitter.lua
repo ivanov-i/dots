0df2405bce202dcb0c2a30f7f3fd3820c53f9168
Igor Ivanov <ivanov.i@gmail.com>
2023-03-07 13:09:05 +0700

    disable treesitter hightlight to avoid w!

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -14,7 +14,8 @@ require'nvim-treesitter.configs'.setup {
 
   highlight = {
     -- `false` will disable the whole extension
-    enable = true,
+	-- disable highlight to avoid requiring w!
+    -- enable = true,
     additional_vim_regex_highlighting = false,
   },
 }
