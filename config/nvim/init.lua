a9c33529c833a59b3c4f1f06d32f3d215741d4e2
Igor Ivanov <ivanov.i@gmail.com>
2023-04-18 11:18:03 +0400

    disable stupid autocomments

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,4 @@
 pcall(require, "impatient")
 require("everything")
+
+vim.api.nvim_create_autocmd("FileType", { callback = function() vim.opt.formatoptions = vim.opt.formatoptions - { "c","r","o" } end, })
