2e58225a7491c799e306d0e8944e97b8756a96fc
Igor Ivanov <ivanov.i@gmail.com>
2023-04-23 14:49:00 +0400

    packer bootstrap

--- config/nvim/lua/everything/packer.lua
+++ config/nvim/lua/everything/packer.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
@@ -80,5 +93,12 @@ return require('packer').startup(function(use)
 	use {'dhruvasagar/vim-prosession', requires= {'tpope/vim-obsession'}}
 	use {'whiteinge/diffconflicts'}
 	use {'vim-test/vim-test'}
+
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
 
