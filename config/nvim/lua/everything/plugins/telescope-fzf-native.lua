9dc935f52b7dbd0d1702aa6a8e770a9bf57b0dc0
Igor Ivanov <ivanov.i@gmail.com>
2023-05-02 09:35:49 +0400

    fix telescope layout

--- config/nvim/lua/everything/plugins/telescope-fzf-native.lua
+++ config/nvim/lua/everything/plugins/telescope-fzf-native.lua
@@ -4,14 +4,6 @@ local x =
 	dependencies = {'nvim-telescope/telescope.nvim'},
 	build = 'make',
 	cond = vim.fn.executable 'make' == 1,
-	opts =
-	{
-		defaults =
-		{
-			layout_strategy = 'vertical',
-			layout_config = { width = 0.999, height = 0.999}
-		},
-	},
 	config = function ()
 		local builtin = require('telescope.builtin')
 		pcall(require('telescope').load_extension, 'fzf')
--- config/nvim/lua/everything/plugins/telescope.lua
+++ config/nvim/lua/everything/plugins/telescope.lua
@@ -2,5 +2,13 @@ return
 {
 	'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	-- or                            , branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	opts =
+	{
+		defaults =
+		{
+			layout_strategy = 'vertical',
+			layout_config = { width = 0.999, height = 0.999}
+		},
+	},
 }
