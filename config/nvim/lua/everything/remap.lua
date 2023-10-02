9b080c5e3c476ec3f76fd58203cd6df7960c7bce
Igor Ivanov <ivanov.i@gmail.com>
2023-10-02 04:30:12 +0400

    idea for nvim

--- config/nvim/lua/everything/remap.lua
+++ config/nvim/lua/everything/remap.lua
@@ -1,21 +1,28 @@
-vim.keymap.set("i", "jj", "<escape>")
-vim.keymap.set("n", "<cr>", ":nohl<cr>")
-vim.keymap.set("n", "<leader>pv", ":Ex<cr>")
-vim.keymap.set("n", "<space>", "<nop>")
+if vim.g.vscode then
+    -- VSCode extension
+else
+    -- ordinary Neovim
 
--- move by screen lines when wrapping
-vim.keymap.set("n", "gj", "j")
-vim.keymap.set("n", "gk", "k")
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
+    vim.keymap.set("i", "jj", "<escape>")
+    vim.keymap.set("n", "<cr>", ":nohl<cr>")
+    vim.keymap.set("n", "<leader>pv", ":Ex<cr>")
+    vim.keymap.set("n", "<space>", "<nop>")
 
--- buffers
-vim.keymap.set("n", "]b", ":bnext<cr>")
-vim.keymap.set("n", "[b", ":bprev<cr>")
-vim.keymap.set("n", "[Q", ":cfirst<cr>")
-vim.keymap.set("n", "]q", ":cnext<cr>")
-vim.keymap.set("n", "[q", ":cprev<cr>")
-vim.keymap.set("n", "]Q", ":clast<cr>")
+    -- move by screen lines when wrapping
+    vim.keymap.set("n", "gj", "j")
+    vim.keymap.set("n", "gk", "k")
+    vim.keymap.set("n", "j", "gj")
+    vim.keymap.set("n", "k", "gk")
+
+    -- buffers
+    vim.keymap.set("n", "]b", ":bnext<cr>")
+    vim.keymap.set("n", "[b", ":bprev<cr>")
+    vim.keymap.set("n", "[Q", ":cfirst<cr>")
+    vim.keymap.set("n", "]q", ":cnext<cr>")
+    vim.keymap.set("n", "[q", ":cprev<cr>")
+    vim.keymap.set("n", "]Q", ":clast<cr>")
+
+end
 
 -- russian
 vim.keymap.set("i", "оо", "<escape>")
