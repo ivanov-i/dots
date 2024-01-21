74a02e2a882511797c4b00372c5382150bf4683d
Igor Ivanov <ivanov.i@gmail.com>
2024-01-21 15:20:14 +0400

    neovim telescope fzf -> zf

--- config/nvim/lua/everything/plugins/telescope-zf-native.lua
+++ config/nvim/lua/everything/plugins/telescope-zf-native.lua
@@ -1,12 +1,9 @@
-local x =
-{
-	'nvim-telescope/telescope-fzf-native.nvim',
+return {
+	'natecraddock/telescope-zf-native.nvim',
 	dependencies = {'nvim-telescope/telescope.nvim'},
-	build = 'make',
-	cond = vim.fn.executable 'make' == 1,
 	config = function ()
 		local builtin = require('telescope.builtin')
-		pcall(require('telescope').load_extension, 'fzf')
+		pcall(require('telescope').load_extension, 'zf-native')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -17,4 +14,3 @@ local x =
 		]])
 	end
 }
-return x
