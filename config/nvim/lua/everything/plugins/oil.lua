49c582ae8f39a98adec556f5cd97c443ff0f49b9
Igor Ivanov <ivanov.i@gmail.com>
2024-04-11 19:14:41 +0300

    auto preview in Oil. disable confirm for simple edits

--- config/nvim/lua/everything/plugins/oil.lua
+++ config/nvim/lua/everything/plugins/oil.lua
@@ -6,5 +6,18 @@ return
     dependencies = {
         "nvim-tree/nvim-web-devicons", -- optional dependency
     },
-    opts = {},
+    config = function()
+        require("oil").setup({
+            skip_confirm_for_simple_edits = false,
+        })
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "OilEnter",
+            callback = vim.schedule_wrap(function(args)
+                local oil = require("oil")
+                if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+                    oil.select({ preview = true })
+                end
+            end),
+        })
+    end,
 }
