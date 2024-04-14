1b051a154324c1d68621c7c7a6f12e3acc71a5f1
Igor Ivanov <ivanov.i@gmail.com>
2024-04-14 23:01:25 +0300

    add highlight on yank in nvim

--- config/nvim/lua/everything/options.lua
+++ config/nvim/lua/everything/options.lua
@@ -119,3 +119,15 @@ vim.opt.guicursor = {
     'o:hor50',
     'a:blinkwait700-blinkoff400-blinkon0-Cursor/lCursor',
 }
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
