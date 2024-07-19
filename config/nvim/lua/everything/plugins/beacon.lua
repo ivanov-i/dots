09249a9864d2003e0cba7032973187eed9938a54
Igor Ivanov <ivanov.i@gmail.com>
2024-07-19 09:08:03 +0400

    fix beacon to highlight cursor on search

--- config/nvim/lua/everything/plugins/beacon.lua
+++ config/nvim/lua/everything/plugins/beacon.lua
@@ -3,10 +3,10 @@ return
 	'danilamihailov/beacon.nvim',
 	init = function()
 		vim.g.beacon_enable = 1
-		vim.keymap.set("n", "n", "n:Beacon<CR>")
-		vim.keymap.set("n", "N", "N:Beacon<CR>")
-		vim.keymap.set("n", "*", "*:Beacon<CR>")
-		vim.keymap.set("n", "#", "#:Beacon<CR>")
+		vim.keymap.set("n", "n", "n:lua require('beacon').highlight_cursor()<CR>Nn")
+		vim.keymap.set("n", "N", "N:lua require('beacon').highlight_cursor()<CR>Nn")
+		vim.keymap.set("n", "*", "*:lua require('beacon').highlight_cursor()<CR>Nn")
+		vim.keymap.set("n", "#", "#:lua require('beacon').highlight_cursor()<CR>Nn")
 
 		vim.api.nvim_command("highlight Beacon guibg=white ctermbg=15")
 	end,
