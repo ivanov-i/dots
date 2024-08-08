6e8de79d249edeb436335737c64e55f7d90b1494
Igor Ivanov <ivanov.i@gmail.com>
2024-08-08 19:39:02 +0400

    fix supermaven keymaps config

--- config/nvim/lua/everything/plugins/supermaven.lua
+++ config/nvim/lua/everything/plugins/supermaven.lua
@@ -1,13 +1,12 @@
 return {
     {
         "supermaven-inc/supermaven-nvim",
-        config = function()
-            require("supermaven-nvim").setup({})
-        end,
-        keymaps = {
-            accept_suggestion = "<leader><Tab>",
-            clear_suggestion = "<C-]>",
-            accept_word = "<C-j>",
+        opts = {
+            keymaps = {
+                accept_suggestion = "<leader><Tab>",
+                clear_suggestion = "<C-]>",
+                accept_word = "<C-j>",
+            },
         },
     },
 }
